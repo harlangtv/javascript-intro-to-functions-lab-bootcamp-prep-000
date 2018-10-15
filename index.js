4a339769c41a6002c42418b892546c72b1f8aed0
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-15 18:37:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function logWhisper(string) {
   return string.toLowerCase()
 }
 fucntion sayHiToGrandma(string) {
-if(string === string.toLowerCase()){
+if(string === string.toLowerCase())
   return("I can't hear you!")}
