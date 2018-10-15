fc2e0ec643ee2544be3c417526650ab9d61a0c9b
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-15 20:09:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
   return("I can't hear you!")
-} else if (string === string.toUpperCase()) {
+} else if(string === string.toUpperCase()) {
      return("YES INDEED!")}
+} else("I love you, Grandma.") {
+  return("I love you, too")
 }
