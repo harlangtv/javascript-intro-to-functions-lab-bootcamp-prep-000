28ac890bbf60bd19863c407df5f6c5ae1bb886ea
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-15 18:45:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function logWhisper(string) {
   console.log('hello')
   return string.toLowerCase()
 }
-fucntion sayHiToGrandma(string){ 
-  if(string === string.toLowerCase())
-  { return("I can't hear you!")}}
+fucntion sayHiToGrandma(string){
+  if(string === string.toLowerCase()){
+  return("I can't hear you!")}}
