a89c6b834c94ff67ccd5e4d9ee0d3270ab9b82c8
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-15 19:10:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
   return("I can't hear you!")}
 }
+function sayHiToGrandma(string){
+  if(string === string.toUpperCase()){
+  return("YES INDEED!")}
+}
